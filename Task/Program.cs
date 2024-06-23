bc2a5acc9c46dfd6e8ae8f080eb1630f63796c9d
aya waheed <ayawaheed1506@gmail.com>
2024-06-24 01:10:11 +0300

    Add project files.

---
+++ Task/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write(" Please Enter Your Name : ");
+            string Name = Console.ReadLine() ?? "NA";
+            Console.Write("Please Enter Your Age : ");
+            int Age;
+            int.TryParse(Console.ReadLine() ?? "0", out Age);
+            Console.Write(" Please Enter Your Salary : ");
+            double Salary;
+            double.TryParse(Console.ReadLine() ?? "0", out Salary);
+            Console.Clear();
+            Console.Write($"Name is  {Name}");
+            Console.Write($"Age is  {Age}");
+            Console.Write($"Salary is  {Salary}");
+            Console.Beep(343, 1000);
+
+
+
+
+        }
+    }
+}
