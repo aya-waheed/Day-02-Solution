650210ccbf23ef49c1c2564ac389637688390e2b
aya waheed <ayawaheed1506@gmail.com>
2024-06-24 14:40:39 +0300

    VideoNumber09

---
+++ Demo/Point.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Point
+    {
+
+        public int X; //4 Bytes
+        public int Y; //4 Bytes
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -24,15 +24,65 @@
 
 
             #region Variable Declaration
-            int Id = 10;
+            //int Id = 10;
 
-            //PascalCase -> FirstName
-            //camelCase -> firstName
-            //kabab-case -> first-name[Angular Files Naming]
-            //snake_case -> first_name
+            ////PascalCase -> FirstName
+            ////camelCase -> firstName
+            ////kabab-case -> first-name[Angular Files Naming]
+            ////snake_case -> first_name
 
-            Console.WriteLine(Id); 
+            //Console.WriteLine(Id);
             #endregion
+
+
+            #region ValueType
+            //int X;
+            ////CLR Will Allocate 4 UnInitialized Bytes at STACK
+            ////int : C# Keyword
+
+            //X = 5;
+
+            //Int32 Y = 10;
+            ////CLR Will Allocate 4 Initialized Bytes at STACK = 10
+            ////Int32 : BCL
+
+            //Y = X; //5
+
+            //X++; //6 
+
+            //Console.WriteLine(Y);
+            #endregion
+
+
+
+            #region ReferenceType
+
+            Point P1;
+            ///Declare of Reference of type "Point" , Refering to NULL
+            ///This Reference 'P1' Can Refer to an Object from type "Point" OR Another type "Inherting from Point"
+            ///CLR Will Allocate 4 Bytes For The Reference at STACK
+            ///CLR Will Allocate 0 Bytes at HEAP
+
+            P1 = new Point() /*Has 2 References[P1 , P2] (اسمين دلع)*/ ; // X = 0 , Y = 0
+            ///new
+            /// 1. Allocate Required Number of Bytes in Heap (Object Size + CLR Overhead Variables)
+            /// 2. Initialize (cross out) Allocated Bytes With Default Values of Its DataType
+            /// 3. Call User-Defined Constructor [if exists]
+            /// 4.Assign The allocated Object to The Reference 'P1'
+
+
+
+            Point P2 = new Point() /*Unreachable Object*/;
+
+            P2 = P1;
+
+            P2.X = 10;
+
+            Console.WriteLine(P1.X); 
+            #endregion
+
+
+
 
         }
     }
