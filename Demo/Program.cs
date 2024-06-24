8b023a8eac03bc2bbc1e375b46cbf38866122d8d
aya waheed <ayawaheed1506@gmail.com>
2024-06-24 03:42:46 +0300

    VideoNumber04

---
+++ Demo/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            #region Comments
+
+            // Single Line Comment
+
+            /*
+             * 
+             * Multi
+             * Line
+             * Comment
+             * 
+             *
+             * 
+             * */
+
+            #endregion
+
+
+            #region Variable Declaration
+            int Id = 10;
+
+            //PascalCase -> FirstName
+            //camelCase -> firstName
+            //kabab-case -> first-name[Angular Files Naming]
+            //snake_case -> first_name
+
+            Console.WriteLine(Id); 
+            #endregion
+
+        }
+    }
+}
